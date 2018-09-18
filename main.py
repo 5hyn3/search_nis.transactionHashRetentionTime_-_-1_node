89011b9eb4cf244f00f3cd0632638c206ed00e44
shyne <shyne9795@gmail.com>
2018-09-18 09:30:37 +0900

    use nemesis blcok

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import requests
 import json
 
-get_transaction_by_hash_path = "/transaction/get?hash=9bacb53783c5fb943797e0b1c096fd5780469734fa58832f9df558ee06427e82"
+get_transaction_by_hash_path = "/transaction/get?hash=e793f3d0c5f01b528a027a6f1a13006685ac88be9d9c67aab29c293f9efa6081"
 
 status_ok_nodes = []
 with open("node_list.json", mode='r', encoding="utf-8_sig") as f:
     nodes = json.load(f)
 
     for node in nodes:
-        address = node["NEM Node"]
-        path = "http://" + address + ":7890" + get_transaction_by_hash_path
+        address = "http://" + node["NEM Node"] + ":" + node["Port"]
+        path = address + get_transaction_by_hash_path
         print(node["#"])
         try:
             if(requests.get(path).status_code == 200):
@@ -19,4 +19,4 @@ with open("node_list.json", mode='r', encoding="utf-8_sig") as f:
 
 with open("result.txt", mode='w') as f:
     for node in status_ok_nodes:
-        f.write(str(node) + "\n")
+        f.write(str(address) + "\n")
